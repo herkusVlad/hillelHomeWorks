e23a15eb907a177d0d8c35d15a2576f3bc250db9
herkusVlad <vlad33732gerkus@gmail.com>
2022-11-14 21:24:12 +0200

    Added homework 10

--- HomeWork10/src/MyArrayList/RunApplication.java
+++ HomeWork10/src/MyArrayList/RunApplication.java
@@ -12,12 +12,14 @@ public class RunApplication {
         list.add("4");
         list.add("5");
 
+        System.out.println("print collection with iterator method : ");
         while (list.hasNext()){
-            System.out.println(list.next());
+            System.out.print(list.next()+" ");
         }
 
         list.remove();
-        System.out.println("List after remove : " + list);
-        System.out.println(list.getUnique());
+        System.out.println("\n\nList after remove with iterator method : " + list);
+
+        System.out.println("\nNew collection with unique value from list : "+list.getUnique());
     }
 }
